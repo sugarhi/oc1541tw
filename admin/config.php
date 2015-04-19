fb2596ec3d6ca3e14f4c389967d2b5efee5c478b
sugarhi <sugarhi0@gmail.com>
2015-04-19 22:38:32 +0800

    Upload opencart_1541 source code

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc1541tw/admin/');
+define('HTTP_CATALOG', 'http://localhost/oc1541tw/');
+define('HTTP_IMAGE', 'http://localhost/oc1541tw/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc1541tw/admin/');
+define('HTTPS_CATALOG', 'http://localhost/oc1541tw/');
+define('HTTPS_IMAGE', 'http://localhost/oc1541tw/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\oc1541tw/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\oc1541tw/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\oc1541tw/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\oc1541tw/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\oc1541tw/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\oc1541tw/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\oc1541tw/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\oc1541tw/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\oc1541tw/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\oc1541tw/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\oc1541tw/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'carry2006');
+define('DB_DATABASE', 'oc1541tw');
+define('DB_PREFIX', 'oc_');
+?>
